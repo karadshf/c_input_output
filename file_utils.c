bb63a9ae0cf4bf3de4dcf7e0e7f066133e6354cf
karadshf <karadshf@email.gvsu.edu>
2017-09-22 11:18:17 -0400

    file_read() and file_write() are now operable.Moving to testing and implementing error handling.

--- file_utils.c
+++ file_utils.c
@@ -5,31 +5,56 @@
 int read_file( char* filename, char **buffer ) {
 
 	FILE* file;
-	file = fopen(filename,"rt"); // Seeks end of file
-	fseek(file, 0L, SEEK_END); // Moves to end of file
-	long size = ftell(file);  // Records size of file
-	rewind(file);				  // Returns to beginning
+	if (!(file = fopen(filename,"rt"))) { // Defines "file" and opens it while checking for errors
+		fprintf(stderr, "Error opening the file. Does \"%s\" exsist?", filename );
+		return -1;
+	}
 
-	char c;
-	int i = 0;
+	fseek(file, 0L, SEEK_END);  // Moves to end of file
+	long size = ftell(file);    // Records size of file
+	rewind(file);			    // Returns to beginning
 
+	int c;						// Placeholder for character reading
+	int i = 0;					// Counter
+
+	*buffer = malloc(1.67 * size * sizeof(char));
+
+	if (*buffer == NULL) {
+		fprintf(stderr, "Error allocating memory to buffer.\n");
+		return -1;
+	}
 	
-	buffer = malloc(1000 * size * sizeof(char));
-	
-	while((c=fgetc(file))!=EOF){
-		printf("%c",c);
-		**(buffer + i) = (char)c;
+	/*
+		Loops until the end of the file is reached
+		Adds a single character to the buffer through each iteration.
+	*/
+	while(!feof(file)) {
+		c=fgetc(file);
+		*((*buffer) + i) = (char)c;
 		i++;
     }
 
-    for(i; i >= 0; i--) {
-    	printf("%c", *(buffer + i));
-    }
-
 	fclose(file);
-    return 0;
+
+    return i;
 }
 
 int write_file( char* filename, char *buffer, int size) {
+
+	FILE* output;
+	if (!(output = fopen(filename, "w"))) { // Defines "output" and opens it while checking or errors
+		fprintf( stderr, "Error creating output file.\n");
+		return -1;
+	}
+
+	/*
+		Prints the buffer in reverse, i.e. the text file in reverse.
+		Substracting 2 is a hack that prevents the printing of two unwanted characters.
+	*/
+	int i = size - 2;
+    for(i; i >= 0; i--) {
+    	//printf("%c", (buffer[i]));
+    	fputc( (buffer[i]), output);
+    }
 
 }
--- reverse.c
+++ reverse.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "file_utils.h"
 
-int main(int* argc, char** argv) {
-	char* buffer = NULL;
-	char* filename = "test.txt";
+int main(int argc, char* argv[]) {
 
-	read_file(filename, &buffer);
+	if (argc < 3) {
+		fprintf( stderr, "Too few arguments. " );
+		fprintf( stderr, "Please format like so:\n./reverse INPUT_FILENAME OUTPUT_FILENAME" );
+		return -1;
+	}
+
+	if (argc > 3) {
+		fprintf( stderr, "Too many arguments. " );
+		fprintf( stderr, "Please format like so:\n./reverse INPUT_FILENAME OUTPUT_FILENAME" );
+		return -1;
+	}
+	
+
+	char* buffer = NULL;
+	char* ipf = argv[1];
+	char* opf = argv[2];
+	int size;
+
+	size = read_file(ipf, &buffer);
+	write_file(opf, buffer, size);
+
+	free(buffer);
 
 	return 0;
 }
